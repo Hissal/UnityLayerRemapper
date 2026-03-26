47e39817370fd3da84ed617c725229cfc998f30a
Lassi Harjaluoma <hissalofficial@gmail.com>
2026-03-26 19:46:35 +0200

    fix: move LayerSnapshotAsst to its own file

--- Assets/LayerRemapper/Editor/LayerMigration/LayerMigrationModels.cs
+++ Assets/LayerRemapper/Editor/LayerMigration/LayerMigrationModels.cs
@@ -30,22 +30,6 @@ namespace LayerRemapper.Editor.LayerMigration {
         }
     }
 
-    /// <summary>Stores a captured copy of the project's layer table for later migration and validation.</summary>
-    public sealed class LayerSnapshotAsset : ScriptableObject {
-        [SerializeField] List<LayerSnapshotEntry> entries = new();
-
-        internal IReadOnlyList<LayerSnapshotEntry> Entries => entries;
-
-        internal void SetEntries(IReadOnlyList<LayerSnapshotEntry> source) {
-            entries.Clear();
-            foreach (var item in source) {
-                var clone = new LayerSnapshotEntry();
-                clone.Set(item.Index, item.Name);
-                entries.Add(clone);
-            }
-        }
-    }
-
     [Serializable]
     internal sealed class LayerRemapEntry {
         [SerializeField] bool enabled = true;
---
+++ Assets/LayerTestSo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LayerTest/LayerTestSo")]
+public class LayerTestSo : ScriptableObject
+{
+    public LayerMask mask;
+    public NestedMaskData nested;
+    
+    public LayerMask[] maskArray;
+    public List<NestedMaskData> nestedList;
+    
+    [SerializeReference] public NestedMaskData referenceNested;
+    [SerializeReference] public List<NestedMaskData> referenceNestedList;
+}
